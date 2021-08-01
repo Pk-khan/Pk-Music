9e58d5a0cf5033812ce555dbfe0f6f61a21ce354
Hemant-sood <hemantsood1999@gmail.com>
2021-08-01 11:08:34 +0530

    deployed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 require('./startup/routes')(app);
 require('./startup/db')();
+require('./startup/prod')(app);
 
 const port = process.env.PORT || 1400;
 app.listen(port, () => { console.log(`Running on ${port}`) });
